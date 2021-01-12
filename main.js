76df644942af62626fb37eadf837eca32fded15c
Max Pereira <max@pereira.at>
2021-01-12 13:58:17 -0800

    Removed context menu option and added extension icon

--- main.js
+++ main.js
@@ -1,18 +1,13 @@
 'use strict';
 
-function getLink({linkUrl}) {
-  fetch(linkUrl + '.json').then(resp =>
-    resp.json().then(pageJson =>
-      window.prompt("Copy to clipboard: Cmd+C, Enter",
-        pageJson[0].data.children[0].data.secure_media.reddit_video.fallback_url + '.gif'
-      )
-    )
-  )
-}
-
-chrome.contextMenus.create({
-  title: "Get direct video link",
-  contexts:["link"],  // ContextType
-  onclick: getLink // A callback function
- });
+chrome.browserAction.onClicked.addListener(function(tab) {
+	chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+		let linkUrl = tabs[0].url;
+		fetch(linkUrl + '.json').then(resp =>
+		resp.json().then(pageJson =>
+			window.prompt("Here's your direct video URL",pageJson[0].data.children[0].data.secure_media.reddit_video.fallback_url)
+		)
+	  )
+	})
+});
 
