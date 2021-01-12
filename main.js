b9bfce05d783b1521894311614748e74df41e1a6
Max Pereira <max@pereira.at>
2021-01-12 14:24:21 -0800

    Work on copying to clipboard

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
 'use strict';
 
+var pageJson = ""
+
+document.addEventListener('copy', function(e) {
+  var textToPutOnClipboard = "test";
+  e.clipboardData.setData('text/plain', textToPutOnClipboard);
+  e.preventDefault();
+});
+
 chrome.browserAction.onClicked.addListener(function(tab) {
 	chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
 		let linkUrl = tabs[0].url;
 		fetch(linkUrl + '.json').then(resp =>
-		resp.json().then(pageJson =>
+		pageJson=resp.json().then(pageJson =>
 			window.prompt("Here's your direct video URL",pageJson[0].data.children[0].data.secure_media.reddit_video.fallback_url)
 		)
 	  )
 	})
+	document.execCommand('copy');
+	console.log(pageJson);
 });
 
